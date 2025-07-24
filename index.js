665e963f298f428a57ea7adce1d668ca6e511093
kate <under5frisk@gmail.com>
2025-07-24 09:43:46 +0300

    beta

--- index.js
+++ index.js
@@ -1,37 +1,77 @@
-const tasksList = document.getElementById("tasklist");
-const tasksField = document.getElementById("tasks-field");
-const dateField = document.getElementById("date-input");
-const timeField = document.getElementById("time-input");
-const submitBtn = document.getElementById("submit-btn"); 
-const heading = document.getElementById("heading");
-const errorUl = document.getElementById("errors_list").querySelector("ul");
-const modalMap = document.getElementById("modal-map");
-const modal = document.getElementById("modal");
-const modalMapBtn = document.getElementById("map-btn");
-const taskTextCls = document.getElementsByClassName("task_text");
+const tasksList = document.getElementById('tasklist');
+const tasksField = document.getElementById('tasks-field');
+const dateField = document.getElementById('date-input');
+const timeField = document.getElementById('time-input');
+const submitBtn = document.getElementById('submit-btn');
+const heading = document.getElementById('heading');
+const errorUl = document.getElementById('errors_list').querySelector('ul');
+const modalMap = document.getElementById('modal-map');
+const modal = document.getElementById('modal');
+const modalMapBtn = document.getElementById('map-btn');
+const taskTextCls = document.getElementsByClassName('task_text');
+const taskArea = document.getElementById('task');
+
+const modalHeading = document.getElementById('modal-heading');
+const modalTime = document.getElementById('modal-time');
+const modalP = document.getElementById('modal-p');
 
 let map;
+let map2;
 
 tasksList.addEventListener('click', (event) => {
   event.stopPropagation();
   if (event.target.classList.contains('task_text')) {
-    modal.classList.remove("hidden");
+    modal.classList.remove('hidden');
+    const idTarget = event.target.closest('li').dataset.id;
+    console.log(idTarget);
+    const currentObjectOfClick = tasksInfoObjs.find(
+      (obj) => obj.id === idTarget
+    );
+    noOpacityColor = currentObjectOfClick.color.replace('0.4', '1');
+    modal.style.borderColor = noOpacityColor;
+    modalHeading.textContent = currentObjectOfClick.heading;
+    modalTime.textContent = currentObjectOfClick.dateTime.replace('T', ' ');
+    modalP.textContent = currentObjectOfClick.textArea;
+    if (currentObjectOfClick.location) {
+      if (map2) {
+        map2.remove();
+      }
+
+      setTimeout(() => {
+        map2 = L.map('map2').setView(
+          [currentObjectOfClick.location[0], currentObjectOfClick.location[1]],
+          13
+        );
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(map2);
+      }, 200);
+    }
   }
-})
+
+  if (event.target.classList.contains('remove_btn')) {
+    event.target.closest('li')?.remove();
+    emptyListBgFuncs.check();
+  }
+});
 
 document.addEventListener('click', (event) => {
   if (!modal.classList.contains('hidden') && !modal.contains(event.target)) {
-    modal.classList.add("hidden");
+    modal.classList.add('hidden');
   }
-})
+});
 
 document.addEventListener('click', (event) => {
-  if (!modalMap.classList.contains("hidden") && !modalMap.contains(event.target)) {
-    modalMap.classList.add("hidden");
+  if (
+    !modalMap.classList.contains('hidden') &&
+    !modalMap.contains(event.target)
+  ) {
+    modalMap.classList.add('hidden');
   }
-})
-
-
+});
 
 let timeNow = new Date();
 console.log(timeNow);
@@ -40,24 +80,24 @@ console.log(timeNow);
 
 const dateTimeArray = [dateField, timeField];
 
-dateTimeArray.forEach(element => {
+dateTimeArray.forEach((element) => {
   element.addEventListener('click', () => {
-  element.showPicker();
-});
+    element.showPicker();
+  });
 });
 
 const emptyListBgFuncs = {
   addBg() {
-    tasksList.classList.add("placeholder_empty");
+    tasksList.classList.add('placeholder_empty');
   },
   removeBg() {
-    tasksList.classList.remove("placeholder_empty");
+    tasksList.classList.remove('placeholder_empty');
   },
 
   check() {
-    (tasksList.children.length < 1) ? this.addBg() : this.removeBg();
-  }
-}
+    tasksList.children.length < 1 ? this.addBg() : this.removeBg();
+  },
+};
 
 function hexToRgba40(hex) {
   if (!/^#([A-Fa-f0-9]{6})$/.test(hex)) {
@@ -69,38 +109,45 @@ function hexToRgba40(hex) {
   return `rgba(${r}, ${g}, ${b}, 0.4)`;
 }
 
+let coords;
+
 modalMapBtn.addEventListener('click', () => {
   event.stopPropagation();
-  modalMap.classList.toggle("hidden");
-  if (modalMap.classList.contains("hidden")) {
+  modalMap.classList.toggle('hidden');
+  if (modalMap.classList.contains('hidden')) {
     return;
   } else {
     if (!map) {
-        map = L.map('map').setView([50.4470, 30.5228], 13); 
-
-      window.coords = undefined;
-
+      map = L.map('map').setView([50.447, 30.5228], 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
 
       var popup = L.popup();
 
       function onMapClick(e) {
-          window.coords = e.latlng;
-          popup
-              .setLatLng(e.latlng)
-              .setContent("You chose location:" + e.latlng.toString())
-              .openOn(map);
+        coords = e.latlng;
+        popup
+          .setLatLng(e.latlng)
+          .setContent('You chose location:' + e.latlng.toString())
+          .openOn(map);
       }
 
       map.on('click', onMapClick);
     }
   }
-})
+});
 
+const generateRandomId = (length = 8) => {
+  return Math.random()
+    .toString(36)
+    .substring(2, length + 2);
+};
+
+const tasksInfoObjs = [];
 
 emptyListBgFuncs.check();
 
@@ -108,41 +155,69 @@ dateField.value = timeNow.slice(0, 10);
 timeField.value = timeNow.slice(11, 16);
 
 document.querySelector('form').addEventListener('submit', (event) => {
-
   event.preventDefault();
   const headingValue = heading.value.trim();
   const timeCombined = `${dateField.value}T${timeField.value}`;
-  const listItem = document.createElement("li");
-  const spanItem = document.createElement("span");
-  const spanItem2 = document.createElement("span");
-  const btn = document.createElement("button");
+  const listItem = document.createElement('li');
+  const spanItem = document.createElement('span');
+  const spanItem2 = document.createElement('span');
+  const btn = document.createElement('button');
   const now = new Date();
   let errorsArray = [];
-  if (new Date(timeCombined) < now) {errorsArray.push("DATE EXPIRED")};
-  if (headingValue.length === 0) {errorsArray.push("HEADING EMPTY")};
+  if (new Date(timeCombined) < now) {
+    errorsArray.push('DATE EXPIRED');
+  }
+  if (headingValue.length === 0) {
+    errorsArray.push('HEADING EMPTY');
+  }
   if (errorsArray.length > 0) {
-    errorUl.innerHTML = "";
-    errorsArray.forEach(error => {
-      const errorLi = document.createElement("li");
+    errorUl.innerHTML = '';
+    errorsArray.forEach((error) => {
+      const errorLi = document.createElement('li');
       errorLi.textContent = error;
       errorUl.appendChild(errorLi);
-    })
+    });
   } else {
-    listItem.classList.add("task_Li");
-    listItem.style.backgroundColor = hexToRgba40(document.getElementById("color-picker").value);
-    btn.classList.add("remove_btn");
-    spanItem2.textContent = heading.value;
-    spanItem2.classList.add("task_text");
-    spanItem.textContent = timeCombined.replace("T", " ");
-    spanItem.classList.add("task_time");
+    const randomId = generateRandomId();
+    const currentObj = {
+      id: randomId,
+      dateTime: timeCombined,
+      heading: heading.value.trim(),
+      textArea: taskArea.value.trim(),
+      color: hexToRgba40(document.getElementById('color-picker').value),
+    };
+    if (coords) {
+      currentObj.location = [coords.lat, coords.lng];
+    } else {
+      currentObj.location = null;
+    }
+
+    tasksInfoObjs.push(currentObj);
+    console.log(tasksInfoObjs);
+
+    listItem.classList.add('task_Li');
+    listItem.style.backgroundColor = currentObj.color;
+    listItem.dataset.id = currentObj.id;
+
+    btn.classList.add('remove_btn');
+
+    spanItem2.textContent = currentObj.heading;
+    spanItem2.classList.add('task_text');
+
+    spanItem.textContent = currentObj.dateTime.replace('T', ' ');
+    spanItem.classList.add('task_time');
+
     listItem.appendChild(spanItem2);
     listItem.appendChild(spanItem);
     listItem.appendChild(btn);
+
     tasksList.appendChild(listItem);
     emptyListBgFuncs.check();
-    document.querySelector("form").reset();
-    window.coords = undefined;
+
+    document.querySelector('form').reset();
+    errorUl.innerHTML = '';
+
     dateField.value = timeNow.slice(0, 10);
     timeField.value = timeNow.slice(11, 16);
-  };
-})
+  }
+});
